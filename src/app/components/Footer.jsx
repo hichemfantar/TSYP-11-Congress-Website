900eb93eee8424d26a1d4abb6028320e0974f847
Hichem Fantar <hichemfantar2049@gmail.com>
2023-04-25 10:37:21 +0100

    hide footer item

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,13 +7,15 @@ export default function Footer() {
 	return (
 		<footer className="container mx-auto p-4 md:px-6 md:py-8">
 			<div className="flex flex-col items-center gap-4 sm:justify-between lg:flex-row">
-				<Link
-					to="https://hichemfantar.netlify.app/"
-					target="_blank"
-					rel="noreferrer"
-					className="flex items-center"
-				>
-					{/* <svg
+				<div>
+					{false && (
+						<Link
+							to="https://hichemfantar.netlify.app/"
+							target="_blank"
+							rel="noreferrer"
+							className="flex items-center"
+						>
+							{/* <svg
 						className="mr-3 h-8 fill-gray-900 dark:fill-gray-100"
 						aria-label="Vercel Logo"
 						// fill="white"
@@ -23,19 +25,21 @@ export default function Footer() {
 					>
 						<path d="M37.59.25l36.95 64H.64l36.95-64z" />
 					</svg> */}
-					{/* <img
+							{/* <img
 						src="https://flowbite.com/docs/images/logo.svg"
 						className="mr-3 h-8"
 						alt="TSYP 11"
 					/> */}
-					<div className="flex flex-wrap gap-2 font-['Tangerine'] text-3xl font-semibold dark:font-normal md:text-4xl ">
-						Brought to life by Hichem Fantar
-						{/* <img src={signature} className="mr-3 h-8 dark:invert" alt="TSYP 11" /> */}
-					</div>
-					{/* <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+							<div className="flex flex-wrap gap-2 font-['Tangerine'] text-3xl font-semibold dark:font-normal md:text-4xl ">
+								Brought to life by Hichem Fantar
+								{/* <img src={signature} className="mr-3 h-8 dark:invert" alt="TSYP 11" /> */}
+							</div>
+							{/* <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
 						TSYP 11
 					</span> */}
-				</Link>
+						</Link>
+					)}
+				</div>
 
 				<div className="hidden gap-4 md:flex">
 					<ul className="ffont-['space_mono'] flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400">
