b4dc547ee69270d4e5904246396d06059196c68a
Hichem Fantar <hichemfantar2049@gmail.com>
2023-06-01 00:52:51 +0100

    schedule timeline

---
+++ src/app/components/timeline-view/TimelineShedule.jsx
@@ -0,0 +1,165 @@
+import React from "react";
+
+import "./TimelineSchedule.scss";
+
+export default function TimelineShedule() {
+	return (
+		<div className="b-our-process service-cloud">
+			<div className="c-process-steps">
+				<div className="step-in">
+					<div className="row step-line">
+						<div className="col-3 corner" />
+						<div className="col-6 corner" />
+					</div>
+				</div>
+				<div className="step">
+					<div className="b-step-content">
+						<div className="b-step-content__logo">
+							<div className="circle">
+								<img
+									src="/themes/satoripop-2019/images/services/identity.svg"
+									alt="Define"
+									data-pagespeed-url-hash={3133138355}
+									onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+								/>
+							</div>
+						</div>
+						<div className="b-step-content__text">
+							<h4 className="b-step-content__title">Define</h4>
+							<div className="b-step-content__description">
+								<p style={{ textAlign: "left" }}>
+									We dive deep into your business needs, requirements and
+									industry specifics to power a custom mobile application
+									strategy.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="row step-line">
+						<div className="col-3 corner" />
+						<div className="col-6 corner-line" />
+						<div className="col-3 corner" />
+					</div>
+				</div>
+				<div className="step">
+					<div className="b-step-content">
+						<div className="b-step-content__logo">
+							<div className="circle">
+								<img
+									src="/themes/satoripop-2019/images/services/smartphone-4.svg"
+									alt="Design"
+									data-pagespeed-url-hash={2635517661}
+									onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+								/>
+							</div>
+						</div>
+						<div className="b-step-content__text">
+							<h4 className="b-step-content__title">Design</h4>
+							<div className="b-step-content__description">
+								<p style={{ textAlign: "left" }}>
+									We craft a unique, interactive design for your application
+									that delivers powerful customer experience and drives user
+									engagement.{" "}
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="row step-line">
+						<div className="col-3 corner" />
+						<div className="col-6 corner-line" />
+						<div className="col-3 corner" />
+					</div>
+				</div>
+				<div className="step">
+					<div className="b-step-content">
+						<div className="b-step-content__logo">
+							<div className="circle">
+								<img
+									src="/themes/satoripop-2019/images/services/smartphone-3.svg"
+									alt="Development"
+									data-pagespeed-url-hash={2341017740}
+									onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+								/>
+							</div>
+						</div>
+						<div className="b-step-content__text">
+							<h4 className="b-step-content__title">Development</h4>
+							<div className="b-step-content__description">
+								<p style={{ textAlign: "left" }}>
+									We use the latest cutting-edge technologies to create advanced
+									and robust custom mobile solutions.{" "}
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="row step-line">
+						<div className="col-3 corner" />
+						<div className="col-6 corner-line" />
+						<div className="col-3 corner" />
+					</div>
+				</div>
+				<div className="step">
+					<div className="b-step-content">
+						<div className="b-step-content__logo">
+							<div className="circle">
+								<img
+									src="/themes/satoripop-2019/images/services/mobile_check.svg"
+									alt="Testing"
+									data-pagespeed-url-hash={172420204}
+									onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+								/>
+							</div>
+						</div>
+						<div className="b-step-content__text">
+							<h4 className="b-step-content__title">Testing</h4>
+							<div className="b-step-content__description">
+								<p style={{ textAlign: "left" }}>
+									Before delivery, our team applies rigorous functionality and
+									usability testing to ensure your mobile application performs
+									optimally.{" "}
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="row step-line">
+						<div className="col-3 corner" />
+						<div className="col-6 corner-line" />
+						<div className="col-3 corner" />
+					</div>
+				</div>
+				<div className="step">
+					<div className="b-step-content">
+						<div className="b-step-content__logo">
+							<div className="circle">
+								<img
+									src="/themes/satoripop-2019/images/services/identity.svg"
+									alt="Maintenance"
+									data-pagespeed-url-hash={3133138355}
+									onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
+								/>
+							</div>
+						</div>
+						<div className="b-step-content__text">
+							<h4 className="b-step-content__title">Maintenance</h4>
+							<div className="b-step-content__description">
+								<p>
+									After the application is released, we provide you with
+									continuous support to improve and scale your mobile
+									application and keep your users engaged.
+									<br />
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="step-out-left">
+					<div className="row step-line">
+						<div className="col-3 corner" />
+						<div className="col-6 corner" />
+						<div className="col-3" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
