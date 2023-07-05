2392c07b31703ff115764bb6cea1f1d1cf876d48
Hichem Fantar <hichemfantar2049@gmail.com>
2023-07-05 22:56:17 +0100

    a

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -13,7 +13,7 @@ export default function Navigation() {
 
 	return (
 		<div>
-			<nav className="rounded border-gray-200 px-2 py-2.5 sm:px-4">
+			<nav className="rounded border-gray-200 px-4 py-2.5 sm:px-8">
 				<div className="container mx-auto flex flex-wrap items-center justify-between">
 					<Link to="/" className="flex items-center">
 						<img
