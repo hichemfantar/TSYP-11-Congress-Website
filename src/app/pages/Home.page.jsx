292a8a4b006143f8826b64a492d457cc9c17ae58
Hichem Fantar <hichemfantar2049@gmail.com>
2024-04-03 23:41:31 +0100

    Add AboutEnetcomSB component

--- src/app/pages/Home.page.jsx
+++ src/app/pages/Home.page.jsx
@@ -282,6 +282,10 @@ export default function HomePage() {
 				<AboutESSTHSSB />
 			</Fade>
 			<Fade triggerOnce>
+				<AboutEnetcomSB />
+			</Fade>
+
+			<Fade triggerOnce>
 				<section className="py-8 md:py-10">
 					<AboutCongress />
 				</section>
@@ -402,6 +406,53 @@ export function AboutESSTHSSB(params) {
 						{/* </div> */}
 					</Link>
 				</div>
+			</div>
+		</section>
+	);
+}
+
+export function AboutEnetcomSB(params) {
+	return (
+		<section className="grid items-center justify-center gap-4 py-8 md:grid-cols-12 md:py-28">
+			<div className="col-span-6">
+				<h1 className="mb-4 text-4xl font-bold tracking-tight   lg:mb-7 lg:text-start lg:text-5xl lg:font-extrabold lg:leading-none">
+					TSYP 12 to be hosted by ENETCOM Student Branch.
+				</h1>
+				<h1 className="mb-4 text-lg font-medium   lg:mb-7 lg:text-start lg:text-xl lg:font-normal">
+					Established on May 8, 2013, the IEEE ENETCOM Student Branch has been
+					steadfast in promoting the IEEE and its mission within the realm of
+					engineering students at ENET'Com. Our core focus is to invest in these
+					students, aiding them in honing their general skills and fostering
+					their development into future leaders and change-makers. With a
+					membership exceeding 472 individuals, including engineers, degree
+					holders, and master's degree holders, our Student Branch proudly
+					stands as the largest in Sfax, ranking 3rd in the Tunisia Section and
+					3rd in Region 8. Our influence extends through 6 Technical Chapters:
+					CAS, CS, IAS, PES, RAS, EMBS, and an Affinity Group: WIE. We take
+					great pride in our global recognition, having received numerous IEEE
+					Awards across various fields. This success is a testament to the
+					dedication and commitment exhibited by each of our members.{" "}
+				</h1>
+				<div>
+					<Link
+						// type="button"
+						to={"https://enetcom.ieee.tn/"}
+						target="_blank"
+						rel="noreferrer"
+						className="relative inline min-w-[120px] items-center justify-center rounded-full border-2 border-gray-600 px-6 py-2 text-center text-sm font-medium transition hover:border-gray-900 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-gray-400 dark:bg-black  dark:hover:bg-white dark:hover:bg-opacity-20 dark:focus:ring-blue-800"
+					>
+						{/* <div className="flex flex-wrap items-center justify-start gap-2"> */}
+						<span className="">Learn more about ENETCOM SB</span>
+						{/* </div> */}
+					</Link>
+				</div>
+			</div>
+			<div className="col-span-6 mx-auto">
+				<CustomImage
+					src={"/assets/enetcomsb.png"}
+					className="mx-auto h-56 rounded-2xl object-contain p-12 sm:h-96"
+					alt="TSYP 11"
+				/>
 			</div>
 		</section>
 	);
